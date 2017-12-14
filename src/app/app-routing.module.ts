9019a08faa2a7ef8e61c949f31531d3ecaa691ee
dakotah martin <dakotah@sidewalkpro.com>
2017-12-13 23:12:22 -0700

    Beginnings of Forums

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { GarageComponent } from './garage/garage.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { GarageComponent } from "./garage/garage.component";
+import { ForumComponent } from "./forum/forum.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/garage', pathMatch: 'full' },
-  { path: 'garage', component: GarageComponent },
+  { path: "", redirectTo: "/garage", pathMatch: "full" },
+  { path: "garage", component: GarageComponent },
+  { path: "forum", component: ForumComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,49 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from "./app-routing.module";
 
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { AppComponent } from './app.component';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireAuthModule } from 'angularfire2/auth';
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { AppComponent } from "./app.component";
+import { AngularFireModule } from "angularfire2";
+import { AngularFirestoreModule } from "angularfire2/firestore";
+import { AngularFireAuthModule } from "angularfire2/auth";
 
-import { environment } from '../environments/environment';
-import { GarageComponent } from './garage/garage.component';
-import { AppService } from './app.service';
+import { MaterialModule } from "./material/material.module";
+
+import { environment } from "../environments/environment";
+import { GarageComponent } from "./garage/garage.component";
+import { AppService } from "./app.service";
+import { SidenavComponent } from "./forum/sidenav/sidenav.component";
+import { ForumComponent } from "./forum/forum.component";
+import { HeaderComponent } from "./forum/header/header.component";
+import { FooterComponent } from "./forum/footer/footer.component";
+import { MessagesContainerComponent } from "./forum/messages-container/messages-container.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    GarageComponent
+    GarageComponent,
+    SidenavComponent,
+    ForumComponent,
+    HeaderComponent,
+    FooterComponent,
+    MessagesContainerComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
-    environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : []
+    environment.production
+      ? ServiceWorkerModule.register("/ngsw-worker.js")
+      : []
   ],
-  providers: [
-    AppService
-  ],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
